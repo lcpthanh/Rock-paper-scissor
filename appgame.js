af47368f6f32ac99c1a58e6dace6c8351d440fc5
Thanh Le <lcpthanh@gmail.com>
2023-02-22 01:51:32 -0500

    Formatting

--- appgame.js
+++ appgame.js
@@ -1,21 +1,21 @@
 const startGame = () => {
-    // console.log("click");
-    document.getElementById("game").style.display = "none";
-    document.getElementById("playGame").style.display = "flex";
-    document.getElementById("startBtn").style.opacity = "0%";
-    document.getElementById("backBtn").style.opacity = "100%";
-    document.querySelector("html").style.backgroundColor = "white";
-    document.getElementById("removeBG").style.background = "none";
-}
+  // console.log("click");
+  document.getElementById("game").style.display = "none";
+  document.getElementById("playGame").style.display = "flex";
+  document.getElementById("startBtn").style.opacity = "0%";
+  document.getElementById("backBtn").style.opacity = "100%";
+  document.querySelector("html").style.backgroundColor = "white";
+  document.getElementById("removeBG").style.background = "none";
+};
 
 const backGame = () => {
-    // console.log("click");
-    document.getElementById("game").style.display = "flex";
-    document.getElementById("playGame").style.display = "none";
-    document.getElementById("startBtn").style.opacity = "100%";
-    document.getElementById("backBtn").style.opacity = "0%";
-    document.querySelector("html").style.backgroundColor = "#E9C93F";
-}
+  // console.log("click");
+  document.getElementById("game").style.display = "flex";
+  document.getElementById("playGame").style.display = "none";
+  document.getElementById("startBtn").style.opacity = "100%";
+  document.getElementById("backBtn").style.opacity = "0%";
+  document.querySelector("html").style.backgroundColor = "#E9C93F";
+};
 
 // Complete logic of game inside this function
 const game = () => {
